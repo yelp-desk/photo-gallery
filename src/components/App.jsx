2272e1e8d6fce471a5dc9d9bf8fbc7ea05d984fb
Alex Harb <alex.harb11@gmail.com>
2019-02-14 15:56:47 -0800

    Implement auto-fade to transition photos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,6 @@ import PhotoSet from './PhotoSet.jsx';
 
 //takes about a second to transition and about every 5-8 seconds is when it does it.
 
-const AppDispatch = React.createContext(null);
-
 function App() {
   const [restaurantId, updateId] =  useState(null); 
   const [photos, updatePhotos] = useState([]);
@@ -19,7 +17,7 @@ function App() {
     updateId(newId);
   }, []) //Runs every page load
   
-  useEffect(() => { //Loads the photos from said restaurant Id
+  useEffect(() => { //Loads the photos from said restaurant Id, will be updated to not use jquery
     $.ajax({
       method: "GET",
       url: `/api/photo-gallery-list/${restaurantId}`,
@@ -34,14 +32,69 @@ function App() {
     })
   }, [restaurantId]) //Only runs when restaurantId is updated
 
-  useEffect(() => { //The array of visible photos
+  useEffect(() => { //The array of all possibly visible photos
     console.log('photos were updated');
     updatePrevs(makePhotoArray(photos.length - 1));
     updateCurrents(makePhotoArray(0));
     updateNexts(makePhotoArray(1));
-  }, [photos]) //updates when photos is updated and when an arrow is clicked due to mainPhoto updating
+  }, [photos]) //updates when photos is updated so that these may render
+
+  useEffect(() => {
+    var timer = setTimeout(() => {
+      let reset = false;
+      let currentView = document.getElementById('currentView');
+      let nextView = document.getElementById('nextView');
+      currentView.style.transitionDuration = "2s";
+      nextView.style.transitionDuration = "2s";
+      currentView.style.opacity = "0";
+      nextView.style.opacity = "1";
+      currentView.addEventListener("transitionend", (event) => {
+        if (!reset) {
+          reset = true;
+          console.log(event.propertyName);
+          currentView.style.transition = "ease 0s";
+          nextView.style.transition = "ease 0s";
+          // currentView.classList.toggle('noTransition');
+          // nextView.classList.toggle('noTransition')
+          // debugger;
+          currentView.innerHTML = '';
+          for (var i = 0; i < 3; i++) {
+            let elementToAdd = nextView.children[i].cloneNode(true);
+            currentView.appendChild(elementToAdd)
+          }
+          currentView.style.opacity = "initial";
+          nextView.style.opacity = "0";
+          // currentView.addEventListener()
+          // currentView.classList.toggle('noTransition');
+          // nextView.classList.toggle('noTransition')
+          shiftNext();
+        }
+      });
+    }, 4000);
+    return () => {
+      // resetOpacity();
+      clearTimeout(timer);
+    }
+  }, [mainPhoto, nextPhotos])
+
+
+  // useEffect(() => {
+  //   resetOpacity();
+  // }, [currentPhotos])
+
+  // function resetOpacity() {
+  //   let current = document.getElementById('currentView');
+  //   current.classList.add('noTransition');
+  //   current.style.opacity = "1";
+  //   current.classList.remove('noTransition');
+  //   let next = document.getElementById('nextView');
+  //   next.classList.add('noTransition');
+  //   next.style.opacity = "0";
+  //   next.classList.remove('noTransition');
+  // }
   
-  function makePhotoArray(photoIndex) {
+  
+  function makePhotoArray(photoIndex) { //Makes an array of 3 photos to be visible via prev, current, or next
     let photoArray;
     if (photoIndex === photos.length - 2) {
       photoArray = photos.slice(photoIndex, photoIndex + 2)
@@ -57,61 +110,52 @@ function App() {
 
   function handleArrowClick(event) {
     if (event.target.className.includes('nextArrow')) {
-      updatePrevs(currentPhotos);
-      updateCurrents(nextPhotos);
-      if (mainPhoto === photos.length - 2) { //I'm sure theres a way to make this shorter, but I'm having a hard time wrapping my head around it rn
-        updateNexts(makePhotoArray(0));
-      } else if (mainPhoto === photos.length - 1) {
-        updateNexts(makePhotoArray(1));
-      } else {
-        updateNexts(makePhotoArray(mainPhoto + 2));
-      }
-      if (mainPhoto === photos.length - 1) {
-        updateMainPhoto(0)
-      } else {
-        updateMainPhoto(mainPhoto + 1)
-      }
+      shiftNext();
     } else {
-      updateNexts(currentPhotos);
-      updateCurrents(prevPhotos);
-      if (mainPhoto >= 2) { //ditto for line 60 
-        updatePrevs(makePhotoArray(mainPhoto - 2));
-      } else if (mainPhoto === 1) {
-        updatePrevs(makePhotoArray(photos.length - 1));
-      } else {
-        updatePrevs(makePhotoArray(photos.length - 2));
-      }
-      if (mainPhoto === 0) {
-        updateMainPhoto(photos.length - 1);
-      } else {
-        updateMainPhoto(mainPhoto - 1);
-      }
+      shiftBack();
     }
   }
 
-  
+  function shiftNext() {
+    updatePrevs(currentPhotos);
+    updateCurrents(nextPhotos);
+    updateNexts(makePhotoArray((mainPhoto + 2) % photos.length))
+    if (mainPhoto === photos.length - 1) {
+      updateMainPhoto(0)
+    } else {
+      updateMainPhoto(mainPhoto + 1)
+    }
+  }
+
+  function shiftBack() {
+    updateNexts(currentPhotos);
+    updateCurrents(prevPhotos);
+    updatePrevs(makePhotoArray((photos.length + (mainPhoto - 2)) % photos.length))
+    if (mainPhoto === 0) {
+      updateMainPhoto(photos.length - 1);
+    } else {
+      updateMainPhoto(mainPhoto - 1);
+    }
+  }
 
   return (
-  <div className="slideShowContainer">
-    <div className="currentView">
+  <div id="slideShowContainer">
+    <div id="currentView">
       {currentPhotos.map((photo, index) => {
-        console.log(currentPhotos.length, 'doing currents')
         return (
           <PhotoSet index={index} photo={photo}/>
         )
       })}   
     </div>
-    <div className="prevView">
+    <div id="prevView">
       {prevPhotos.length === 3 && prevPhotos.map((photo, index) => { //for some reason, it won't do prevs on its own, so i added the conditional
-        console.log(prevPhotos.length, 'doing prevs')
         return (
           <PhotoSet index={index} photo={photo}/>
         )
       })}
     </div>
-    <div className="nextView">
+    <div id="nextView">
       {nextPhotos.map((photo, index) => {
-        console.log(nextPhotos.length, 'doing nexts')
         return (
           <PhotoSet index={index} photo={photo}/>
           )
